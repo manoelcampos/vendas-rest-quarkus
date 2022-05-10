99ef9f9102d1eba81c4aa8a7c47228a93f457a88
Manoel Campos <manoelcampos@gmail.com>
2022-05-10 17:22:22 -0300

    Adiciona atributo "preco" no produto e inclui novas validações do Hibernate Validator.

--- src/main/java/com/manoelcampos/model/Produto.java
+++ src/main/java/com/manoelcampos/model/Produto.java
@@ -1,19 +1,25 @@
 package com.manoelcampos.model;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Entity
 public class Produto extends PanacheEntity {
     @Column(unique = true, nullable = false)
+    @NotBlank
     private String descricao;
 
     @NotNull
     @ManyToOne
     private Categoria categoria;
+
+    @NotNull
+    @Positive
+    private double preco;
 
     public Long getId() {
         return id;
@@ -37,5 +43,13 @@ public class Produto extends PanacheEntity {
 
     public void setCategoria(Categoria categoria) {
         this.categoria = categoria;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
     }
 }
