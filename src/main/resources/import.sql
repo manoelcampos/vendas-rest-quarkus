afb40a556ac98a6a077cdfa63f2f7e6045ba8fe3
Manoel Campos <manoelcampos@gmail.com>
2022-05-10 18:03:33 -0300

    Atualiza script de importação de dados para o banco.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,1 @@
-insert into Categoria (descricao) values ('Eletrônicos'), ('Roupas'), ('Brinquedos'), ('Alimentos');
+insert into categoria values (1, 'Eletrônicos'), (2, 'Roupas'), (3, 'Brinquedos'), (4, 'Alimentos');
