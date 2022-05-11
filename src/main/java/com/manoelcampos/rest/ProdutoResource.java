e9a42e265f80f64ce3a5e84dfe3118fbd3c1d614
Manoel Campos <manoelcampos@gmail.com>
2022-05-11 17:57:12 -0300

    Adiciona o ProdutoResource e popula a tabela respectiva.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

---
+++ src/main/java/com/manoelcampos/rest/ProdutoResource.java
@@ -0,0 +1,32 @@
+package com.manoelcampos.rest;
+
+import com.manoelcampos.model.Produto;
+
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/produto")
+public class ProdutoResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Produto> list() {
+        return Produto.listAll();
+    }
+
+    @GET()
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Produto findById(@PathParam("id") final long id) {
+        return Produto.findById(id);
+    }
+
+    @POST()
+    @Produces(MediaType.APPLICATION_JSON)
+    @Transactional
+    public void insert(final Produto Produto) {
+        Produto.persistAndFlush();
+    }
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,5 @@
-insert into categoria values (1, 'Eletrônicos'), (2, 'Roupas'), (3, 'Brinquedos'), (4, 'Alimentos');
+insert into categoria values
+            (1, 'Eletrônicos'), (2, 'Smartphones'), (3, 'Roupas'),
+            (4, 'Brinquedos'), (5, 'Alimentos');
+insert into produto (id, descricao, preco, categoria_id)  values
+            (1, 'TV', 2000, 1), (2, 'iPhone', 4000, 2);
