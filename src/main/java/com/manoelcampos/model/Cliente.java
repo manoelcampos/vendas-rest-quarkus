909703a594a27c4cf7ac771d04b356ff427c0ae6
Manoel Campos <manoelcampos@gmail.com>
2022-10-17 17:35:53 -0300

    Adiciona @Entity na classe Cliente, exigido mesmo estendendo PanacheEntity.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/manoelcampos/model/Cliente.java
+++ src/main/java/com/manoelcampos/model/Cliente.java
@@ -4,10 +4,12 @@ import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
+@Entity
 public class Cliente extends PanacheEntity {
     @NotNull @NotBlank
     String nome;
