8852ff6fe1e0b7c7ba154f8b523db555272e8180
Manoel Campos <manoelcampos@gmail.com>
2022-11-07 16:18:27 -0300

    Mostra como seria a adição de métodos default em interfaces que implementam PanacheEntityResource
    Esta é apenas uma ideia, mas o método getByName
    é simplesmente ignorado e o endpoint
    para ele não é gerado.
    
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/manoelcampos/rest/ClienteResource.java
+++ src/main/java/com/manoelcampos/rest/ClienteResource.java
@@ -4,6 +4,11 @@ import com.manoelcampos.model.Cliente;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
 import io.quarkus.rest.data.panache.MethodProperties;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
 /**
  * Usa a biblioteca quarkus-hibernate-orm-rest-data-panache para criar um
  * REST Resource automaticamente.
@@ -16,4 +21,15 @@ public interface ClienteResource extends PanacheEntityResource<Cliente, Long> {
      */
     @MethodProperties(exposed = false)
     boolean delete(long id);
+
+    /**
+     * Seria ótimo se funcionasse, mas o método é ignorado e não é gerado um endpoint.
+     * @param name
+     * @return
+     */
+    @GET
+    @Path("/name/{name}")
+    default Cliente getByName(@PathParam("name") String name){
+        return Cliente.findByName(name).orElseThrow(NotFoundException::new);
+    }
 }
