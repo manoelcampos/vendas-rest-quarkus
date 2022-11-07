29747d9756cde8081c1f838ee5d3a1d9c6d87ee4
Manoel Campos <manoelcampos@gmail.com>
2022-11-07 15:15:47 -0300

    Deixa de expor a operação delete para o ClienteResource.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/manoelcampos/rest/ClienteResource.java
+++ src/main/java/com/manoelcampos/rest/ClienteResource.java
@@ -2,11 +2,18 @@ package com.manoelcampos.rest;
 
 import com.manoelcampos.model.Cliente;
 import io.quarkus.hibernate.orm.rest.data.panache.PanacheEntityResource;
+import io.quarkus.rest.data.panache.MethodProperties;
 
 /**
  * Usa a biblioteca quarkus-hibernate-orm-rest-data-panache para criar um
  * REST Resource automaticamente.
  */
 public interface ClienteResource extends PanacheEntityResource<Cliente, Long> {
-
+    /**
+     * Não expõe a operação delete, impedindo que clientes sejam removidos pela API web.
+     * @param id
+     * @return
+     */
+    @MethodProperties(exposed = false)
+    boolean delete(long id);
 }
